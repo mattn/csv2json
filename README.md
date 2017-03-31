5236c97d458c0b6fe37a89d5b8c7970ebe6498fa
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-03-31 17:35:28 +0900

    first import

---
+++ README.md
@@ -0,0 +1,23 @@
+# csv2json
+
+CSV/JSON converter
+
+## Usage
+
+```
+$ cat mytweets.csv | csv2json -f text
+```
+
+## Installation
+
+```
+$ go get github.com/mattn/csv2json
+```
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a. mattn)
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+var (
+	header = flag.Bool("header", true, "use header")
+	field  = flag.String("field", "", "collect field")
+	raw    = flag.Bool("raw", false, "raw string")
+	obj    = flag.Bool("obj", false, "output object")
+)
+
+func fatal(err error) {
+	fmt.Fprintf(os.Stderr, "%s: %s", os.Args[0], os.Stderr, err)
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+
+	if *obj && !*header {
+		fatal(errors.New("-obj and -header both should be specified"))
+	}
+	if *obj && *raw {
+		fatal(errors.New("-obj and -raw both should not be specified"))
+	}
+
+	var ff []string
+	if *field != "" {
+		ff = strings.Split(*field, ",")
+	}
+
+	fm := make(map[string]int)
+	csvr := csv.NewReader(os.Stdin)
+
+	if *header {
+		record, err := csvr.Read()
+		if err != nil {
+			fatal(err)
+		}
+		for i, v := range record {
+			fm[v] = i
+		}
+	}
+
+	first := true
+	for {
+		record, err := csvr.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fatal(err)
+		}
+
+		if first {
+			first = false
+			if !*raw {
+				fmt.Print("[")
+			}
+			if !*header {
+				for i := 0; i < len(record); i++ {
+					fm[fmt.Sprintf("f%d", i+1)] = i
+				}
+			}
+		} else if !*raw {
+			fmt.Print(",")
+		}
+
+		if *obj {
+			out := make(map[string]string)
+			if len(ff) > 0 {
+				for _, fi := range ff {
+					i := fm[fi]
+					out[fi] = record[i]
+				}
+			} else {
+				for k, v := range fm {
+					out[k] = record[v]
+				}
+			}
+			b, err := json.Marshal(out)
+			if err != nil {
+				fatal(err)
+			}
+			fmt.Print(string(b))
+		} else {
+			if len(ff) > 0 {
+				out := []string{}
+				for _, fi := range ff {
+					i := fm[fi]
+					out = append(out, record[i])
+				}
+				record = out
+			}
+			if *raw {
+				fmt.Print(strings.Join(record, ""))
+			} else {
+				b, err := json.Marshal(record)
+				if err != nil {
+					fatal(err)
+				}
+				fmt.Print(string(b))
+			}
+		}
+	}
+	if !*raw {
+		fmt.Print("]")
+	}
+}
